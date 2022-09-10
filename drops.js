130d17383fd4c1c7ca4ba04cc206741a5b17d68e
zhangxuanyu2019 <57307624+zhangxuanyu2019@users.noreply.github.com>
2022-09-10 13:30:44 +0800

    Delete drops.js

--- drops.js
+++
@@ -1,4 +0,0 @@
-hostname = api-prod.languagedrops.com
-
-# > 修复Drops解锁(2022.02.09)
-^https\:\/\/api-prod\.languagedrops\.com\/syncPurchasesV\d url script-response-body https://raw.githubusercontent.com/langkhach270389/Quantumult-X-LK/master/Scripts/langkhach/drops.js
