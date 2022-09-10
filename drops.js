5bf1a06f833603abf87d613594c36d872bc3f41f
zhangxuanyu2019 <57307624+zhangxuanyu2019@users.noreply.github.com>
2022-09-10 19:03:27 +0800

    Update drops.js

--- drops.js
+++ drops.js
@@ -1,3 +1,7 @@
+
+
+^https\:\/\/api-prod\.languagedrops\.com\/syncPurchasesV\d url script-response-body https://raw.githubusercontent.com/zhangxuanyu2019/xuanyu/main/drops.js
+
 var obj = JSON.parse($response.body);
 
 obj= {
