51b598c3cc8d08d68d5ade49cefd34aef1c08b37
zhangxuanyu2019 <57307624+zhangxuanyu2019@users.noreply.github.com>
2022-11-19 14:31:03 +0800

    Update xmind.js

--- xmind.js
+++ xmind.js
@@ -12,7 +12,7 @@ obj = {
   "raw_data": "S0MY6Wu5wpkW52RE5XmMkSMfTBvnytTwIJODrtVDjnA0axrORbnv9gh1RC4W3/ejTfQhNBb7CVxxpbYnBBk2tHc4gAODhsuGpHkltYNL/P5dfORSpdbiNkAZr5aBBbHS/dNlaYjLYyBkq9Ohfe0QS9PeXOWLbDdNA6kqidLJysw=",
   "license": {
     "status": "sub",
-    "expireTime": 9999999999999
+    "expireTime": 8999999999999
   },
   "_code": 200
 }
