54aca78a8eac4cf103baa7fec3095d28ec5f811b
zhangxuanyu2019 <57307624+zhangxuanyu2019@users.noreply.github.com>
2022-11-24 19:25:43 +0800

    Update Craft.js

--- Craft.js
+++ Craft.js
@@ -1,21 +1,13 @@
 /******************************
 
 脚本功能：Craft - 文件及笔记编辑器——解锁订阅
-下载地址：https://is.gd/ngDZcJ
-软件版本：2.3.6
-脚本作者：彭于晏💞
-更新时间：2022-10-20
-问题反馈：QQ+89996462
-QQ会员群：779392027💞
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py996
 使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *******************************
 
 [rewrite_local]
 
-^https:\/\/api\.craft\.do\/auth\/v2\/profile url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Craft.js
+^https:\/\/api\.craft\.do\/auth\/v2\/profile url script-response-body https://raw.githubusercontent.com/zhangxuanyu2019/xuanyu/Surge/Craft.js
 
 [mitm] 
 
